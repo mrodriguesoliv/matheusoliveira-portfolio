fdbd5574370485510361e4a5efdc8d10973045bb
Matheus Oliveira <168552816+mrodriguesoliv@users.noreply.github.com>
2025-08-29 13:21:39 +0000

    Implement code changes to enhance functionality and improve performance

--- src/config/index.ts
+++ src/config/index.ts
@@ -35,50 +35,53 @@ export const SITE_CONTENT: SiteContent = {
       startDate: "Apr 2024",
       endDate: "Present",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        "Backend development with Node.js (TypeScript) and Python (Django, Flask, FastAPI)",
+        "Integration of solutions using OpenAI and Gemini APIs.",
+        "Creation of interactive web applications using React, NextJS, Angular.",
+        "Monitoring of QA and production deployments, with integration with CI/CD pipelines.",
+        "Clean Code practices and Design Patterns.",
+        "Active participation in Daily, Sprint and Plannings."
       ],
     },
     {
       company: "Barra Mansa Alimentos",
       position: "Maintenance Coordinator",
       startDate: "Jan 2021",
-      endDate: "May 2018",
+      endDate: "May 2025",
       summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
+        "Managing multidisciplinary teams and conducting continuous improvement projects.",
+        "Structuring and standardizing processes with a focus on operational efficiency and scalability.",
+        "Participation in industrial automation projects, working on mapping and defining processes with the programming team to implement automated solutions.",
+        "Planning and implementation of new systems and equipment, defining scope, schedule, and performance KPIs."
+      ],
+    },
+    {
+      company: "Reis Advogados",
+      position: "Estrategist",
+      startDate: "May 2019",
+      endDate: "Jan 2021",
+      summary: [
+        "Development of reporting applications using VBA and Python,mensuring reproducibility and efficiency.",
+        "Creation of algorithms to optimize the daily performance of employees.",
+        "Defining strategies and designing databases for analysis and decision-making.",
+        "Data mapping and modeling to support the business."
       ],
     },
   ],
   projects: [
     {
-      name: "Spotifu Music",
-      summary: "A music streaming app that emulates Spotify's core features.",
+      name: "TrafficAlerts Waze Clone",
+      summary: "A traffic monitoring API that transforms real-time driver reports into immediate alerts for supervisors.",
       linkPreview: "/",
       linkSource: "https://github.com/immois/astro-zen",
       image: "/spotifu.png",
     },
-    {
-      name: "Shopp App",
-      summary: "An e-commerce platform that replicates Shopify's key features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/shopify-clon.png",
-    },
-    {
-      name: "ClonTagram",
-      summary: "A social network that replicates the features of Instagram",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/clone-ig.png",
-    },
   ],
   about: {
     description: `
-      Hi, I’m Matheus Oliveira, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
+     I'm a developer with a background in engineering and a passion for building robust and user-friendly web applications. With my experience in coordinating industrial projects, I've developed a strong ability to manage and deliver efficient solutions.
 
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+    I've honed my skills in creating and maintaining web applications, developing responsive interfaces, and integrating APIs. My expertise extends to programming, code versioning, and technical troubleshooting. I'm committed to best development practices, code quality, and delivering effective solutions that not only meet user needs but also push the boundaries of what's possible.
     `,
     image: "/Matheus Oliveira-big.jpg",
   },
